a627ba33b2bba8abf79325d340e737c6afc43fa7
Rafael Alexander Reyes Pérez <rafaelalexanderr5@gmail.com>
2022-04-11 11:31:48 -0400

    [ADD] cryptoNews api and layout

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -15,7 +15,7 @@ export const cryptoNewsApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptoNews: builder.query({
-      query: (newsCategory, count) =>
+      query: ({ newsCategory, count }) =>
         createRequest(
           `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
         ),
